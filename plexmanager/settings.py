2d1c03e21d26efe088cb99229c4ff8fae7bf1487
Dmitry Butrimov <d.butrimov@gmail.com>
2020-03-27 12:52:19 +0300

    support ENV_FILE env variable

--- plexmanager/settings.py
+++ plexmanager/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 env = environ.Env()
 
-env_file = os.path.join(BASE_DIR, '.env')
+env_file = env.str('ENV_FILE', default=os.path.join(BASE_DIR, '.env'))
 if os.path.isfile(env_file):
     environ.Env.read_env(env_file=env_file)  # reading .env file
 
